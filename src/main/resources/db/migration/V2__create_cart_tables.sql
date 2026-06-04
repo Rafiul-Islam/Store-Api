3dfe9b8174cee0254fb21d46e40e2a94d19e5f93
Rafiul Islam <rafi.cse.ru@gmail.com>
2026-06-04 22:41:15 +0600

    feat(cart): add Cart and CartItem entities with database schema

---
+++ src/main/resources/db/migration/V2__create_cart_tables.sql
@@ -0,0 +1,16 @@
+create table carts
+(
+    id           BINARY(16) default (UUID_TO_BIN(UUID())) not null primary key,
+    date_created DATE       default (CURDATE())           not null
+);
+
+create table cart_items
+(
+    id         bigint auto_increment primary key,
+    cart_id    BINARY(16)    not null,
+    product_id bigint        not null,
+    quantity   int default 1 not null,
+    constraint cart_items_cart_product_unique unique (cart_id, product_id),
+    constraint cart_items_carts_id_fk foreign key (cart_id) references carts (id) on delete cascade,
+    constraint cart_items_products_id_fk foreign key (product_id) references products (id) on delete cascade
+);
